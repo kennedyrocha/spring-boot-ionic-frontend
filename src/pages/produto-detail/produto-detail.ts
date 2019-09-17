1355d6d31c16a6967bb2364981f68d1ed8ff8303
unknown <kennedy_snp@hotmail.com>
2019-09-17 02:23:01 -0400

    Carregando dados do produto

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Item } from 'ionic-angular';
 import { produtoDTO } from '../../models/produto.dto';
+import { ProdutoService } from '../../services/domain/produto.service';
+import { API_CONFIG } from '../../config/api.config';
 
 
 @IonicPage()
@@ -12,15 +14,27 @@ export class ProdutoDetailPage {
 
   item : produtoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public produtoService : ProdutoService) {
   }
 
   ionViewDidLoad() {
-    this.item = {
-      id: "1",
-      nome: "Mouse",
-      preco: 80.5
-    }
+    let id = this.navParams.get('produto_id');
+    this.produtoService.findById(id)
+      .subscribe(response => {
+        this.item = response;
+        this.getImageUrlIfExists();
+      },
+      error => {});
   }
 
+  getImageUrlIfExists() {
+    this.produtoService.getImageFromBucket(this.item.id)
+      .subscribe(response => {
+        this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
+      },
+      error => {});
+  }
 }
